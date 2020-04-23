56d37966b1c0fff97f0096237714c1c04ed1761d
yuxu <woniuatyu@163.com>
2020-04-23 12:32:00 +0800

    Initial commit

---
+++ test/test.py
@@ -0,0 +1,16 @@
+# -*-coding:utf-8-*-
+import sys
+import os
+
+# print(os.getcwd())
+# file = open("/Users/yuxu/PycharmProjects/05_nb_dt_lr/test/abc", "r")
+# b = True
+# while b:
+#     content = file.readline()
+#     if len(content) == 0:
+#         b = False
+#     print(content)
+
+z = {'name': 1, "age": "abc", 2: 2, 3: {"a": 11, "b": 22}}
+print(3 not in z)
+print(z[3])
